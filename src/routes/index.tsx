3dffc3c2bd9b0a0367daca76688678d5ec365a83
oswin902m <1665434994@qq.com>
2026-05-23 17:01:03 +0800

    chore(init): setup shadcn

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,3 +1,4 @@
+import { Button } from '@/components/ui/button'
 import { createFileRoute } from '@tanstack/react-router'
 
 export const Route = createFileRoute('/')({ component: Home })
@@ -9,6 +10,7 @@ function Home() {
       <p className="mt-4 text-lg">
         Edit <code>src/routes/index.tsx</code> to get started.
       </p>
+      <Button className="mt-4">Click me</Button>
     </div>
   )
 }
