37737266774c52619f8822f6598b56cad58c0ba9
oswin902m <1665434994@qq.com>
2026-05-23 17:50:43 +0800

    feat(ui:auth-form): login / signup form with chrome validation

--- src/routes/_auth/login/index.tsx
+++ src/routes/_auth/login/index.tsx
@@ -1,9 +1,10 @@
 import { createFileRoute } from '@tanstack/react-router'
+import { LoginForm } from './-components/login-form'
 
 export const Route = createFileRoute('/_auth/login/')({
   component: RouteComponent,
 })
 
 function RouteComponent() {
-  return <div>Hello "/_auth/login/"!</div>
+  return <LoginForm />
 }
--- src/routes/_auth/signup/index.tsx
+++ src/routes/_auth/signup/index.tsx
@@ -1,9 +1,10 @@
 import { createFileRoute } from '@tanstack/react-router'
+import { SignupForm } from './-components/signup-form'
 
 export const Route = createFileRoute('/_auth/signup/')({
   component: RouteComponent,
 })
 
 function RouteComponent() {
-  return <div>Hello "/_auth/signup/"!</div>
+  return <SignupForm />
 }
